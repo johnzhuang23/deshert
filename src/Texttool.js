c3377231d0282eb14ebba5b165bcfad0455d0252
johnzz <zhuangzhong23@gmail.com>
2022-02-09 17:06:24 +1100

    draggable text

---
+++ src/Texttool.js
@@ -0,0 +1,14 @@
+
+export default function Texttools() {
+
+    return (
+        <section className="picture">
+            <h2>design your text </h2>
+            <input type="text" onChange={handleChange} placeholder="say something" />
+            <h1>{content}</h1>
+
+        </section>
+
+    )
+}
+
